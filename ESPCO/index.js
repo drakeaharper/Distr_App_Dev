0440023bafc045559c6b2790897aa5cc5362927c
Drake Harper <10660212@uvu.edu>
2020-08-31 22:48:28 -0600

    ESPCO basics done

--- ESPCO/index.js
+++ ESPCO/index.js
@@ -1,3 +1,42 @@
-var cardTypes = [ 'Circle', 'Square', 'Star', 'Plus', 'Wave' ]
+var cardTypes = [ 'circle', 'square', 'star', 'plus', 'wave' ]
 var winCondition = 5
+var correctAnswers = 0
 var roundTracker = 0
+var numberOfRounds = 10
+var currentCard = _.sample(cardTypes)
+console.log(`current card: ${currentCard}`)
+updateDiplays()
+
+function initializeGame() {
+    roundTracker = 0
+    correct_answers = 0
+    // document.querySelector('#start-btn').innerHTML = 'Reset Game'
+}
+
+function playRound(guess) {
+    if (roundTracker < numberOfRounds) {
+        console.log({card: currentCard, player_guess: guess, round: roundTracker})
+        if (guess === currentCard) {
+            correctAnswers++
+            new Audio('./correct.mp3').play()
+        } else {
+            window.navigator.vibrate(200)
+        }
+        roundTracker++
+        currentCard = _.sample(cardTypes)
+        console.log(`current card: ${currentCard}`)
+        updateDiplays()
+    } else {
+        if (correctAnswers >= winCondition) {
+            new Audio('./winner.mp3').play()
+        } else {
+            new Audio('./lose_sound.mp3').play()
+
+        }
+    }
+}
+
+function updateDiplays() {
+    document.querySelector('#score-display').innerHTML = `Score: ${correctAnswers}/${numberOfRounds}`
+    document.querySelector('#round-display').innerHTML = `Round: ${roundTracker}/${numberOfRounds}`
+}
